9ac920ca75cc00739c5b665d009fd12609534c80
Naisila Puka <37271756+NaisilaPuka@users.noreply.github.com>
2018-04-02 20:38:37 +0300

    Create Try.java

---
+++ Try.java
@@ -0,0 +1,16 @@
+/**
+ * __A class to try our game so far. You will have to download the zip file :) :) :)
+ * @author __Naisila Puka___
+ * @version __30/04/2017__
+ */
+import java.awt.*;
+import java.io.*;
+import javax.swing.*;
+public class Try
+{
+  public static void main(String[] args) throws IOException, InterruptedException
+  {
+    LaunchFrame frame = new LaunchFrame();
+     frame.setVisible(true);
+  }
+}
